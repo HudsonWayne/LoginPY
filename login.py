916f391ade50e0487f2c5d86a9980cf846d749bc
HudsonWayne <waynewaynebenhura@gmail.com>
2025-04-15 13:28:58 +0200

    adding logging in system

--- login.py
+++ login.py
@@ -8,6 +8,10 @@ def main_screen():
     screen.geometry("1280x720+150+80")
     screen.configure(bg="#d7dae2")
     
+    
     image_icon = PhotoImage(file= "icon.png")
+    screen.iconphoto(False,image_icon)
+    
+    screen.title("Login System")
     
 main_screen()
