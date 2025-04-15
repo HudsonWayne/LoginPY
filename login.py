ec4fc1df08ef48a86fedf82f12c57483064301df
HudsonWayne <waynewaynebenhura@gmail.com>
2025-04-15 12:01:33 +0200

    added main screen

--- login.py
+++ login.py
@@ -1,3 +1,11 @@
 from tkinter import *
 from tkinter import messagebox
 import os 
+
+
+def main_screen():
+    screen = Tk()
+    screen.geometry("1280x720+150+80")
+    screen.configure(bg="#d7dae2")
+    
+main_screen()
