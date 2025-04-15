c17a6a0923065949fbe5fb3cdb57ea5046190dd7
HudsonWayne <waynewaynebenhura@gmail.com>
2025-04-15 14:19:38 +0200

    added the frame

--- login.py
+++ login.py
@@ -17,5 +17,8 @@ def main_screen():
     lblTitle=Label(text = "Login System",font = ("Arial",50,'bold'),fg="black", bg="#d7dae2")
     lblTitle.pack(pady=50)
     bordercolor = Frame(screen,bg="black",width=800, height=400)
+    bordercolor.pack()
     
+    mainframe=Frame(bordercolor, bg="#d7dae2",width=800, height=400)
+    mainframe.pack(padx=20, pady=20)
 main_screen()
